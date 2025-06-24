49c010adf13a03ee0f3765de4b10f5e8638a2e24
katsudon16 <sucipto.kathleen@gmail.com>
2025-06-23 21:12:00 -0400

    Add chapter 10: sum reduction with shared memory

---
+++ chapter_10/sum_reduction_shared_memory.cu
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+#define INPUT_SIZE 20
+// BLOCK_SIZE = ceil(INPUT_SIZE / 2.0).
+#define BLOCK_SIZE 10
+
+
+/**
+ *  Perform sum reduction with shared memory, corresponds to Fig. 10.11.
+ */
+__global__
+void SumReductionSharedMemory(
+    float* input_array,
+    float* output_value
+) {
+    __shared__ float input_shared[BLOCK_SIZE];
+    unsigned int index = threadIdx.x;
+    input_shared[index] = input_array[index] + input_array[index + BLOCK_SIZE];
+
+    // For handling odd size values.
+    unsigned int previous_stride = INPUT_SIZE;
+    for (unsigned int stride = ceil(blockDim.x / 2.0); stride >= 1; stride = ceil(stride / 2.0)) {
+        __syncthreads();
+        if (threadIdx.x + stride < previous_stride) {
+            input_shared[index] += input_shared[index + stride];
+        }
+        // Since ceil(stride / 2.0) = 1, we need to ensure that the loops eventually ends.
+        if (stride == 1)
+            break;
+        previous_stride = stride;
+    }
+    if (threadIdx.x == 0) {
+        *output_value = input_shared[0];
+    }
+}
+
+void runSumReduction(
+    float* input_array_h,
+    float* output_value_h
+) {
+    // Get size in bytes.
+    size_t size_input = INPUT_SIZE * sizeof(float);
+    size_t size_output = sizeof(float);
+
+    // Load and copy input_array and filter to device memory.
+    float * input_array_d, * output_value_d;
+    
+    cudaMalloc((void***)&input_array_d, size_input);
+    cudaMemcpy(input_array_d, input_array_h, size_input, cudaMemcpyHostToDevice);
+
+    cudaMalloc((void***)&output_value_d, size_output);
+
+    // Invoke kernel.
+    dim3 dimBlock(BLOCK_SIZE);
+    dim3 dimGrid(1);
+    SumReductionSharedMemory<<<dimGrid, dimBlock>>>(input_array_d, output_value_d);
+
+    // Copy the output matrix from the device memory.
+    cudaMemcpy(output_value_h, output_value_d, size_output, cudaMemcpyDeviceToHost);
+
+    // Free device vectors.
+    cudaFree(input_array_d);
+    cudaFree(output_value_d);
+}
+
+
+int main() {
+    unsigned int input_size = INPUT_SIZE;
+    float input_array[input_size];
+    float output_value;
+
+    for (int i = 1; i < input_size + 1; ++i) 
+        input_array[i-1] = i;
+
+    runSumReduction(
+        input_array,
+        &output_value
+    );
+
+    printf("%.0f\n", output_value);
+
+    return 0;
+}
