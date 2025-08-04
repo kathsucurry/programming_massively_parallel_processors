5e5e7190f8f6b90aeede24ac049d3792fb4b30c8
katsudon16 <sucipto.kathleen@gmail.com>
2025-08-04 15:11:57 -0400

    Add chapter 15: bfs bottom up

--- chapter_15/bfs_bottomup.cu
+++ chapter_15/bfs_bottomup.cu
@@ -1,5 +1,5 @@
 /**
- *  Perform BFS top down, corresponds to chapter 15.3 (Fig. 15.6).
+ *  Perform BFS bottom up, corresponds to chapter 15.3 (Fig. 15.8).
  */
 #include <stdio.h>
 #include <limits.h>
@@ -12,46 +12,46 @@
 
 __global__
 void BfsTopDownKernel(
-    unsigned int *src_pointer_indices,
-    unsigned int *dst_indices,
+    unsigned int *dst_pointer_indices,
+    unsigned int *src_indices,
     unsigned int *level,
     unsigned int current_level,
     unsigned int *new_vertex_visited
 ) {
     unsigned int vertex = blockIdx.x * blockDim.x + threadIdx.x;
-    if (vertex < VERTEX_LENGTH && level[vertex] == current_level - 1) {
-        for (unsigned int edge = src_pointer_indices[vertex];
-            edge < src_pointer_indices[vertex + 1]; ++edge) {
-                unsigned int neighbor = dst_indices[edge];
-                // Ignore if already visited.
-                if (level[neighbor] != UINT_MAX)
-                    continue;
-                level[neighbor] = current_level;
-                new_vertex_visited[0] = 1;
+    if (vertex < VERTEX_LENGTH && level[vertex] == UINT_MAX) {
+        for (unsigned int edge = dst_pointer_indices[vertex];
+            edge < dst_pointer_indices[vertex + 1]; ++edge) {
+                unsigned int neighbor = src_indices[edge];
+                if (level[neighbor] == current_level - 1) {
+                    level[vertex] = current_level;
+                    new_vertex_visited[0] = 1;
+                    break;
+                }
         }
     }
 }
 
 
 void runBFS(
-    unsigned int *csr_src_pointer_indices_h,
-    unsigned int *csr_dst_indices_h,
+    unsigned int *csc_dst_pointer_indices_h,
+    unsigned int *csc_src_indices_h,
     unsigned int edge_length,
     unsigned int *level_h,
     unsigned int root
 ) {
     // Load and copy host variables to device memory.
-    unsigned int *csr_src_pointer_indices_d, *csr_dst_indices_d, *level_d, *new_vertex_visited_d;
+    unsigned int *csc_dst_pointer_indices_d, *csc_src_indices_d, *level_d, *new_vertex_visited_d;
 
-    size_t size_src_pointer_indices = (VERTEX_LENGTH + 1) * sizeof(unsigned int);
-    size_t size_dst_indices = edge_length * sizeof(unsigned int);
+    size_t size_dst_pointer_indices = (VERTEX_LENGTH + 1) * sizeof(unsigned int);
+    size_t size_src_indices = edge_length * sizeof(unsigned int);
     size_t size_vertex = VERTEX_LENGTH * sizeof(unsigned int);
 
-    cudaMalloc((void**)&csr_src_pointer_indices_d, size_src_pointer_indices);
-    cudaMemcpy(csr_src_pointer_indices_d, csr_src_pointer_indices_h, size_src_pointer_indices, cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&csc_dst_pointer_indices_d, size_dst_pointer_indices);
+    cudaMemcpy(csc_dst_pointer_indices_d, csc_dst_pointer_indices_h, size_dst_pointer_indices, cudaMemcpyHostToDevice);
 
-    cudaMalloc((void**)&csr_dst_indices_d, size_dst_indices);
-    cudaMemcpy(csr_dst_indices_d, csr_dst_indices_h, size_dst_indices, cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&csc_src_indices_d, size_src_indices);
+    cudaMemcpy(csc_src_indices_d, csc_src_indices_h, size_src_indices, cudaMemcpyHostToDevice);
 
     cudaMalloc((void**)&level_d, size_vertex);
     // Initialize with "not visited" value.
@@ -76,8 +76,8 @@ void runBFS(
         cudaMemset(new_vertex_visited_d, 0, sizeof(unsigned int));
 
         BfsTopDownKernel<<<dimGrid, dimBlock>>>(
-            csr_src_pointer_indices_d,
-            csr_dst_indices_d,
+            csc_dst_pointer_indices_d,
+            csc_src_indices_d,
             level_d,
             current_level,
             new_vertex_visited_d
@@ -93,24 +93,24 @@ void runBFS(
     cudaMemcpy(level_h, level_d, size_vertex, cudaMemcpyDeviceToHost);
 
     // Free device arrays.
-    cudaFree(csr_src_pointer_indices_d);
-    cudaFree(csr_dst_indices_d);
+    cudaFree(csc_dst_pointer_indices_d);
+    cudaFree(csc_src_indices_d);
     cudaFree(level_d);
     cudaFree(new_vertex_visited_d);
 }
 
 
 int main() {
-    // Generate the CSR graph; all values are 1s.
-    unsigned int src_pointer_indices[] = {0, 2, 4, 7, 9, 11, 12, 13, 15, 15};
-    unsigned int dst_indices[] = {1, 2, 3, 4, 5, 6, 7, 4, 8, 5, 8, 6, 8, 0, 6};
+    // Generate the CSC graph; all values are 1s.
+    unsigned int dst_pointer_indices[] = {0, 1, 2, 3, 4, 6, 8, 11, 12, 15};
+    unsigned int src_indices[] = {7, 0, 0, 1, 1, 3, 2, 4, 2, 5, 7, 2, 3, 4, 6};
     unsigned int edge_length = 15;
 
     unsigned int root = 2;
     unsigned int *level = (unsigned int*)malloc(VERTEX_LENGTH * sizeof(unsigned int));
     runBFS(
-        src_pointer_indices,
-        dst_indices,
+        dst_pointer_indices,
+        src_indices,
         edge_length,
         level,
         root
